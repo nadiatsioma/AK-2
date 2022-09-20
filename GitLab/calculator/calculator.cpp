8e4c404c537bd24214209004df709e62f9d0a420
nadiatsioma <99203179+nadiatsioma@users.noreply.github.com>
2022-09-20 20:44:33 +0300

    Add files via upload

---
+++ GitLab/calculator/calculator.cpp
@@ -0,0 +1,17 @@
+#include "calculator.h"
+
+int Calculator::Add (int a, int b)
+{
+    return a + b;
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+//калькулятор 
+//ка
+//ль
+//ку
+//ля
+//тор
---
+++ calculator/calculator.cpp
@@ -0,0 +1,12 @@
+#include "calculator.h"
+
+int Calculator::Add (double a, double b)
+{
+	return a + b;
+
+}
+
+int Calculator::Sub (double a, double b)
+{
+	return Add (a, -b);
+}
